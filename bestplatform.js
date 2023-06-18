8ffc385f05146d55fe20a5b7bbb19897c8f3c7af
JiaxuanOng <ong.jiaxuan0219@gmail.com>
2023-06-18 14:07:25 +0800

    tooltip graph

--- bestplatform.js
+++ bestplatform.js
@@ -44,7 +44,97 @@ function calculateBestPlatform(data) {
         .attr("width", 300)
         .attr("height", 90)
         .attr("x", 30) // Set the x-coordinate for centering the logo
-        .attr("y", 10);
+        .attr("y", 10)
+        .on("mouseover", function () {
+            tooltip.style("visibility", "visible");
+    
+            // Remove any existing content inside the tooltip
+            tooltip.html("");
+    
+            // Create a bar chart inside the tooltip
+            var platformData = Array.from(platformCounts, ([platform, count]) => ({
+              platform,
+              count,
+            }));
+    
+           // Create a bar chart inside the tooltip
+        var tooltipSvg = tooltip
+        .append("svg")
+        .attr("width", 450)
+        .attr("height", 200);
+
+        var margin = { top: 10, right: 10, bottom: 30, left: 60 };
+        var width = 400 - margin.left - margin.right;
+        var height = 200 - margin.top - margin.bottom;
+
+        var x = d3
+        .scaleLinear()
+        .domain([0, d3.max(platformData, (d) => d.count)])
+        .range([0, width]);
+
+        var y = d3
+        .scaleBand()
+        .domain(platformData.map((d) => d.platform))
+        .range([height, 0])
+        .padding(0.1);
+
+        var xAxis = d3.axisBottom(x);
+        var yAxis = d3.axisLeft(y);
+
+        var chart = tooltipSvg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        chart.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+        chart.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        chart.selectAll(".bar")
+        .data(platformData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("y", (d) => y(d.platform))
+        .attr("width", (d) => x(d.count))
+        .attr("height", y.bandwidth())
+        .attr("fill", (d, i) => colorScale(i));
+
+        chart.selectAll(".bar-label")
+        .data(platformData)
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", (d) => x(d.count) + 5)
+        .attr("y", (d) => y(d.platform) + y.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .text((d) => d.count)
+        .style("font-size", "10px")
+        .attr("fill", "black");
+
+        tooltipSvg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .text("Total Shows per Platform")
+        .style("font-size", "15px")
+        .attr("fill", "black");
+
+          })
+          .on("mousemove", function () {
+            tooltip
+              .style("top", d3.pointer(event)[1] + 10 + "px")
+              .style("left", d3.pointer(event)[0] + 800 + "px");
+          })
+          .on("mouseout", function () {
+            tooltip.style("visibility", "hidden");
+          });
     } else if (bestPlatform === "amazon") {
       svgBP
         .append("image")
@@ -54,16 +144,95 @@ function calculateBestPlatform(data) {
         .attr("x", 30) // Set the x-coordinate for centering the logo
         .attr("y", 10)
         .on("mouseover", function () {
-          tooltip.style("visibility", "visible").html("Total Shows in " + bestPlatform + ": "  + distinctTitles.length);
-        })
-        .on("mousemove", function (event) {
-          tooltip
-            .style("top", d3.pointer(event)[1] + window.pageYOffset + 10 + "px")
-            .style("left", d3.pointer(event)[0] + window.pageXOffset + 900 + "px");
-        })
-        .on("mouseout", function () {
-          tooltip.style("visibility", "hidden");
-        });
+            tooltip.style("visibility", "visible");
+    
+            // Remove any existing content inside the tooltip
+            tooltip.html("");
+    
+            // Create a bar chart inside the tooltip
+            var platformData = Array.from(platformCounts, ([platform, count]) => ({
+              platform,
+              count,
+            }));
+    
+           // Create a bar chart inside the tooltip
+        var tooltipSvg = tooltip
+        .append("svg")
+        .attr("width", 450)
+        .attr("height", 200);
+
+        var margin = { top: 10, right: 10, bottom: 30, left: 60 };
+        var width = 400 - margin.left - margin.right;
+        var height = 200 - margin.top - margin.bottom;
+
+        var x = d3
+        .scaleLinear()
+        .domain([0, d3.max(platformData, (d) => d.count)])
+        .range([0, width]);
+
+        var y = d3
+        .scaleBand()
+        .domain(platformData.map((d) => d.platform))
+        .range([height, 0])
+        .padding(0.1);
+
+        var xAxis = d3.axisBottom(x);
+        var yAxis = d3.axisLeft(y);
+
+        var chart = tooltipSvg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        chart.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+        chart.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        chart.selectAll(".bar")
+        .data(platformData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("y", (d) => y(d.platform))
+        .attr("width", (d) => x(d.count))
+        .attr("height", y.bandwidth())
+        .attr("fill", (d, i) => colorScale(i));
+ 
+        chart.selectAll(".bar-label")
+        .data(platformData)
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", (d) => x(d.count) + 5)
+        .attr("y", (d) => y(d.platform) + y.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .text((d) => d.count)
+        .style("font-size", "10px")
+        .attr("fill", "black");
+
+        tooltipSvg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .text("Total Shows per Platform")
+        .style("font-size", "15px")
+        .attr("fill", "black");
+
+          })
+          .on("mousemove", function () {
+            tooltip
+              .style("top", d3.pointer(event)[1] + 10 + "px")
+              .style("left", d3.pointer(event)[0] + 800 + "px");
+          })
+          .on("mouseout", function () {
+            tooltip.style("visibility", "hidden");
+          });
     } else if (bestPlatform === "disney") {
       svgBP
         .append("image")
@@ -71,7 +240,99 @@ function calculateBestPlatform(data) {
         .attr("width", 300)
         .attr("height", 100)
         .attr("x", 30) // Set the x-coordinate for centering the logo
-        .attr("y", 10);
+        .attr("y", 10)
+        .on("mouseover", function () {
+            tooltip.style("visibility", "visible");
+    
+            // Remove any existing content inside the tooltip
+            tooltip.html("");
+    
+            // Create a bar chart inside the tooltip
+            var platformData = Array.from(platformCounts, ([platform, count]) => ({
+              platform,
+              count,
+            }));
+    
+           // Create a bar chart inside the tooltip
+        var tooltipSvg = tooltip
+        .append("svg")
+        .attr("width", 450)
+        .attr("height", 200);
+
+        var margin = { top: 10, right: 10, bottom: 30, left: 60 };
+        var width = 400 - margin.left - margin.right;
+        var height = 200 - margin.top - margin.bottom;
+
+        var x = d3
+        .scaleLinear()
+        .domain([0, d3.max(platformData, (d) => d.count)])
+        .range([0, width]);
+
+        var y = d3
+        .scaleBand()
+        .domain(platformData.map((d) => d.platform))
+        .range([height, 0])
+        .padding(0.1);
+
+        var xAxis = d3.axisBottom(x);
+        var yAxis = d3.axisLeft(y);
+
+        var chart = tooltipSvg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        chart.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+        chart.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        chart.selectAll(".bar")
+        .data(platformData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("y", (d) => y(d.platform))
+        .attr("width", (d) => x(d.count))
+        .attr("height", y.bandwidth())
+        .attr("fill", (d, i) => colorScale(i));
+
+        
+        
+        chart.selectAll(".bar-label")
+        .data(platformData)
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", (d) => x(d.count) + 5)
+        .attr("y", (d) => y(d.platform) + y.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .text((d) => d.count)
+        .style("font-size", "10px")
+        .attr("fill", "black");
+
+        tooltipSvg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .text("Total Shows per Platform")
+        .style("font-size", "15px")
+        .attr("fill", "black");
+
+          })
+          .on("mousemove", function () {
+            tooltip
+              .style("top", d3.pointer(event)[1] + 10 + "px")
+              .style("left", d3.pointer(event)[0] + 800 + "px");
+          })
+          .on("mouseout", function () {
+            tooltip.style("visibility", "hidden");
+          });
     } else if (bestPlatform === "hulu") {
       svgBP
         .append("image")
@@ -79,7 +340,99 @@ function calculateBestPlatform(data) {
         .attr("width", 300)
         .attr("height", 100)
         .attr("x", 30) // Set the x-coordinate for centering the logo
-        .attr("y", 10);
+        .attr("y", 10)
+        .on("mouseover", function () {
+            tooltip.style("visibility", "visible");
+    
+            // Remove any existing content inside the tooltip
+            tooltip.html("");
+    
+            // Create a bar chart inside the tooltip
+            var platformData = Array.from(platformCounts, ([platform, count]) => ({
+              platform,
+              count,
+            }));
+    
+           // Create a bar chart inside the tooltip
+        var tooltipSvg = tooltip
+        .append("svg")
+        .attr("width", 450)
+        .attr("height", 200);
+
+        var margin = { top: 10, right: 10, bottom: 30, left: 60 };
+        var width = 400 - margin.left - margin.right;
+        var height = 200 - margin.top - margin.bottom;
+
+        var x = d3
+        .scaleLinear()
+        .domain([0, d3.max(platformData, (d) => d.count)])
+        .range([0, width]);
+
+        var y = d3
+        .scaleBand()
+        .domain(platformData.map((d) => d.platform))
+        .range([height, 0])
+        .padding(0.1);
+
+        var xAxis = d3.axisBottom(x);
+        var yAxis = d3.axisLeft(y);
+
+        var chart = tooltipSvg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        chart.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+        chart.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        chart.selectAll(".bar")
+        .data(platformData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("y", (d) => y(d.platform))
+        .attr("width", (d) => x(d.count))
+        .attr("height", y.bandwidth())
+        .attr("fill", (d, i) => colorScale(i));
+
+        
+        
+        chart.selectAll(".bar-label")
+        .data(platformData)
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", (d) => x(d.count) + 5)
+        .attr("y", (d) => y(d.platform) + y.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .text((d) => d.count)
+        .style("font-size", "10px")
+        .attr("fill", "black");
+
+        tooltipSvg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .text("Total Shows per Platform")
+        .style("font-size", "15px")
+        .attr("fill", "black");
+
+          })
+          .on("mousemove", function () {
+            tooltip
+              .style("top", d3.pointer(event)[1] + 10 + "px")
+              .style("left", d3.pointer(event)[0] + 800 + "px");
+          })
+          .on("mouseout", function () {
+            tooltip.style("visibility", "hidden");
+          });
     }
   }
   
