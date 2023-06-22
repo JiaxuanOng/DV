160c751bef4cd33dfe20d9372b256b65f9ff2924
JiaxuanOng <ong.jiaxuan0219@gmail.com>
2023-06-23 01:58:49 +0800

    pie

--- pie.js
+++ pie.js
@@ -117,8 +117,16 @@ d3.csv('StreamingPlatform.csv').then(function(data) {
             if (platform.key === "Disney+") {
                 tooltipPie
                     .style("top", (y + 500) + "px") // Adjust the y position specifically for "Disney+" TV show
-                    .style("left", (x + 100) + "px");
-            } else {
+                    .style("left", (x + 200) + "px");
+            }
+            
+            else if (platform.key === "Netflix") {
+                tooltipPie
+                    .style("top", (y + 500) + "px") // Adjust the y position specifically for "Disney+" TV show
+                    .style("left", (x + 300) + "px");
+            }
+            
+            else {
                 tooltipPie
                     .style("top", (y + 500) + "px")
                     .style("left", (x + 200) + "px");
