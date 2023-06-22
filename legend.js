ae41ccac79b2549ce9d20410504abf772c212e77
JiaxuanOng <ong.jiaxuan0219@gmail.com>
2023-06-23 04:32:50 +0800

    highlight

--- legend.js
+++ legend.js
@@ -1,29 +1,90 @@
 // Define the legend data
 var legendData = [
-    { platform: "Netflix", color: "#d62728" },    
-    { platform: "Disney+", color:  "#1f77b4"},
-    { platform: "Amazon Prime", color: "#ff7f0e"},
-    { platform: "Hulu", color:  "#2ca02c" }
-  ];
-  
-  // Create the legend container
-  var legendContainer = d3.select("#legendPContainer");
-  
-  // Create the legend items
-  var legendItems = legendContainer.selectAll(".legend-item")
-    .data(legendData)
-    .enter()
-    .append("div")
-    .attr("class", "legend-item");
-  
-  // Add color squares to the legend items
-  legendItems.append("div")
-    .attr("class", "legend-color")
-    .style("background-color", d => d.color);
-  
-  // Add platform labels to the legend items
-  legendItems.append("div")
-  .attr("class", "legend-label")
-  .style("color", d => d.color)
-  .text(d => d.platform);
+  { platform: "Netflix", color: "#d62728" },    
+  { platform: "Amazon Prime", color: "#ff7f0e" }, 
+   { platform: "Disney+", color: "#1f77b4" },
+  { platform: "Hulu", color: "#2ca02c" }
+];
 
+// Create the legend container
+var legendContainer = d3.select("#legendPContainer");
+
+// Create the legend items
+var legendItems = legendContainer
+  .selectAll(".legend-item")
+  .data(legendData)
+  .enter()
+  .append("div")
+  .attr("class", (d) => "legend-item " + d.platform.replace(/\s/g, '').replace("+", "\\+"));
+
+
+
+// Add color squares to the legend items
+legendItems
+  .append("div")
+  .attr("class", "legend-color")
+  .style("background-color", (d) => d.color);
+
+// Add platform labels to the legend items
+legendItems
+  .append("div")
+  .attr("class", "legend-label")
+  .style("color", (d) => d.color)
+  .text((d) => d.platform);
+
+// Add event listeners to the legend items
+
+legendItems.on("click", function (event, d) {
+  var platform = d.platform;
+  var selectedLegend = d3.select(this);
+
+  // Toggle the highlight class on the clicked legend item
+  var isActive = selectedLegend.classed("highlight");
+  selectedLegend.classed("highlight", !isActive);
+
+  // Toggle the highlight class on the corresponding graph elements
+  d3.selectAll(".bar." + platform.replace("+", "\\+")).classed("highlight", !isActive);
+
+  console.log(platform);
+  highlightPlatform(platform);
+});
+
+
+// Click event handler for legend items
+function legendClickHandler(clickedData) {
+  // Get the platform of the clicked legend item
+  var clickedPlatform = clickedData.platform;
+
+  // Highlight the clicked platform in the other graphs
+  highlightPlatform(clickedPlatform);
+}
+// Function to highlight the selected platform in other graphs
+function highlightPlatform(platform) {
+  // Check if the clicked legend item has the highlight class
+  var isActive = d3.select(".legend-item." + platform.replace(/\s/g, '').replace("+", "\\+")).classed("highlight");
+
+  // Set the opacity of the pie chart elements based on whether the clicked legend item is active
+  d3.select("#pie1").selectAll("path").style("opacity", (d) =>
+    d.data.platform === platform ? (isActive ? 1 : (isActive ? 0.1 : 1)) : (isActive ? 0.1 : 1)
+  );
+  d3.select("#pie2").selectAll("path").style("opacity", (d) =>
+    d.data.platform === platform ? (isActive ? 1 : (isActive ? 0.1 : 1)) : (isActive ? 0.1 : 1)
+  );
+  d3.select("#pie3").selectAll("path").style("opacity", (d) =>
+    d.data.platform === platform ? (isActive ? 1 : 0.1) : (isActive ? 0.1 : 1)
+  );
+  d3.select("#pie4").selectAll("path").style("opacity", (d) =>
+    d.data.platform === platform ? (isActive ? 1 : 0.1) : (isActive ? 0.1 : 1)
+  );
+
+  // Set the opacity and stroke width of the stacked bar chart elements based on whether the clicked legend item is active
+  d3.select("#stackedBar")
+    .selectAll("rect")
+    .style("opacity", (d) => (d.platform === platform ? (isActive ? 1 : (isActive ? 0.1 : 1)) : (isActive ? 0.1 : 1)))
+    .style("stroke-width", (d) => (d.platform === platform ? (isActive ? 2 : (isActive ? 0.1 : 2)) : (isActive ? 0.1 : 2)));
+
+  // Set the opacity of the line chart elements based on whether the clicked legend item is active
+  d3.select("#lineMT")
+    .selectAll(".line")
+    .style("opacity", (d) => (d.platform === platform ? (isActive ? 1 : (isActive ? 0.1 : 1)) : (isActive ? 0.1 : 1)));
+}
--- stackedBar.js
+++ stackedBar.js
@@ -1,139 +1,138 @@
 d3.csv("StreamingPlatform.csv").then(function (data) {
-    // Filter the data for the first 5 genres
-    var filteredData = data.filter(d => ["Top1", "Top2", "Top3", "Top4", "Top5"].includes(d.TopNGenre));
-  
-    // Group the data by genre and platform
-    var groupedData = d3.group(filteredData, d => d.listed_in, d => d.Platform);
-  
-    // Calculate the count of each platform for each genre
-    var stackedData = Array.from(groupedData, ([genre, platformData]) => ({
-      genre: genre,
-      platforms: Array.from(platformData, ([platform, movies]) => ({
-        platform: platform,
-        count: movies.length
-      }))
-    }));
-  
-    // Sort the platforms within each genre by count in descending order
-    stackedData.forEach(genreData => {
-      genreData.platforms.sort((a, b) => b.count - a.count);
-      genreData.platforms = genreData.platforms.slice(0, 5); // Keep only the top 5 platforms
-    });
-  
-    // Define platform colors
-    var color = d3.scaleOrdinal()
-    .domain(["Netflix",  "Disney+","Amazon Prime", "Hulu"])
-    .range(["#d62728", "#1f77b4","#ff7f0e", "#2ca02c"]);
-  
-    // Function to generate the HTML markup for the pie chart
-function createPieChartHTML(data, genre) {
-    var radius = 100;
-    var diameter = radius * 2;
-  
-    var svg = `<svg width="${diameter}" height="${diameter}" class="pie-chartSB">`;
-  
-    var pie = d3.pie()
-      .value(d => d.count)
-      .sort(null);
-  
-    var arc = d3.arc().innerRadius(0).outerRadius(radius);
-  
-    var arcs = pie(data);
-  
-    var colorScale = d3.scaleOrdinal()
-      .domain(data.map(d => d.platform))
-      .range(["#d62728", "#ff7f0e","#1f77b4", "#2ca02c"]);
-    
-    svg += `<g transform="translate(${radius},${radius})">`;
-    svg += `<text x="0" y="-${radius}" text-anchor="middle" class="genre-title">ooooo</text>`;
-    svg += arcs
-      .map(d => {
-        var path = arc(d);
-        return `<path d="${path}" fill="${colorScale(d.data.platform)}"></path>`;
-      })
-      .join("");
-    svg += "</g>";
-    svg += "</svg>";
-  
-    return svg;
-  }
-    // Set up the chart dimensions
-    var margin = {top: 10, right: 10, bottom: 25, left: 55};
-    var width = 430 - margin.left - margin.right;
-    var height = 480 - margin.top - margin.bottom;
+  // Filter the data for the first 5 genres
+  var filteredData = data.filter(d => ["Top1", "Top2", "Top3", "Top4", "Top5"].includes(d.TopNGenre));
 
-    // Create the SVG container
-    var svgSB = d3.select("#stackedBar")
-      .append("svg")
-      .attr("width", 100 + width + margin.left + margin.right)
-      .attr("height", 100+height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-    // Set up the scales
-    var x = d3.scaleBand()
-      .domain(stackedData.map(d => d.genre))
-      .range([0, width + 100])
-      .padding(0.1);
-  
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(stackedData, d => d3.max(d.platforms, p => p.count))])
-      .range([height, 0]);
-  
-    var tooltipSB = d3.select("body").append("div")
-      .attr("class", "tooltipSB")
-      .style("opacity", 0);
-  
-    // Create the stacked bars
-    svgSB.selectAll(".stack")
-      .data(stackedData)
-      .enter()
-      .append("g")
-      .attr("class", "stack")
-      .attr("transform", d => "translate(" + x(d.genre) + ",0)")
-      .selectAll("rect")
-      .data(d => d.platforms)
-      .enter()
-      .append("rect")
-      .attr("x", d => x.bandwidth() / 4)
-      .attr("y", d => y(d.count))
-      .attr("width", x.bandwidth() / 2)
-      .attr("height", d => height - y(d.count))
-      .style("fill", d => color(d.platform))
-      .on("mouseover", function (event, d) {
-        tooltipSB.transition()
-          .duration(200)
-          .style("opacity", .9);
-  
-        var genre = d3.select(this.parentNode).datum().genre;
-        var platformData = stackedData.find(g => g.genre === genre).platforms;
-        var platformDistribution = platformData.map(p => ({ platform: p.platform, count: p.count }));
-  
-        tooltipSB.html(createPieChartHTML(platformDistribution, genre))
-          .style("left", (event.pageX) + "px")
-          .style("top", (event.pageY - 28) + "px");
-      })
-      .on("mouseout", function (d) {
-        tooltipSB.transition()
-          .duration(500)
-          .style("opacity", 0);
-      });
-  
-    // Add the x-axis
-    svgSB.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .style("fill", "white")
-      .attr("transform", "rotate(-30)")
-      .style("text-anchor", "end")
-      .attr("dx", "-0.8em")
-      .attr("dy", "0.15em");
-  
-    // Add the y-axis
-    svgSB.append("g")
-      .attr("class", "axis")
-      .call(d3.axisLeft(y));
+  // Group the data by genre and platform
+  var groupedData = d3.group(filteredData, d => d.listed_in, d => d.Platform);
+
+  // Calculate the count of each platform for each genre
+  var stackedData = Array.from(groupedData, ([genre, platformData]) => ({
+    genre: genre,
+    platforms: Array.from(platformData, ([platform, movies]) => ({
+      platform: platform,
+      count: movies.length
+    }))
+  }));
+
+  // Sort the platforms within each genre by count in descending order
+  stackedData.forEach(genreData => {
+    genreData.platforms.sort((a, b) => b.count - a.count);
+    genreData.platforms = genreData.platforms.slice(0, 5); // Keep only the top 5 platforms
   });
+
+  // Define platform colors
+  var color = d3.scaleOrdinal()
+  .domain(["Netflix",  "Disney+","Amazon Prime", "Hulu"])
+  .range(["#d62728", "#1f77b4","#ff7f0e", "#2ca02c"]);
+
+  // Function to generate the HTML markup for the pie chart
+function createPieChartHTML(data, genre) {
+  var radius = 100;
+  var diameter = radius * 2;
+
+  var svg = `<svg width="${diameter}" height="${diameter}" class="pie-chartSB">`;
+
+  var pie = d3.pie()
+    .value(d => d.count)
+    .sort(null);
+
+  var arc = d3.arc().innerRadius(0).outerRadius(radius);
+
+  var arcs = pie(data);
+
+  var colorScale = d3.scaleOrdinal()
+    .domain(data.map(d => d.platform))
+    .range(["#d62728", "#ff7f0e","#1f77b4", "#2ca02c"]);
   
+  svg += `<g transform="translate(${radius},${radius})">`;
+  svg += `<text x="0" y="-${radius}" text-anchor="middle" class="genre-title">ooooo</text>`;
+  svg += arcs
+    .map(d => {
+      var path = arc(d);
+      return `<path d="${path}" fill="${colorScale(d.data.platform)}"></path>`;
+    })
+    .join("");
+  svg += "</g>";
+  svg += "</svg>";
+
+  return svg;
+}
+  // Set up the chart dimensions
+  var margin = {top: 10, right: 10, bottom: 25, left: 55};
+  var width = 430 - margin.left - margin.right;
+  var height = 480 - margin.top - margin.bottom;
+
+  // Create the SVG container
+  var svgSB = d3.select("#stackedBar")
+    .append("svg")
+    .attr("width", 100 + width + margin.left + margin.right)
+    .attr("height", 100+height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Set up the scales
+  var x = d3.scaleBand()
+    .domain(stackedData.map(d => d.genre))
+    .range([0, width + 100])
+    .padding(0.1);
+
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(stackedData, d => d3.max(d.platforms, p => p.count))])
+    .range([height, 0]);
+
+  var tooltipSB = d3.select("body").append("div")
+    .attr("class", "tooltipSB")
+    .style("opacity", 0);
+
+  // Create the stacked bars
+  svgSB.selectAll(".stack")
+    .data(stackedData)
+    .enter()
+    .append("g")
+    .attr("class", "stack")
+    .attr("transform", d => "translate(" + x(d.genre) + ",0)")
+    .selectAll("rect")
+    .data(d => d.platforms)
+    .enter()
+    .append("rect")
+    .attr("x", d => x.bandwidth() / 4)
+    .attr("y", d => y(d.count))
+    .attr("width", x.bandwidth() / 2)
+    .attr("height", d => height - y(d.count))
+    .style("fill", d => color(d.platform))
+    .on("mouseover", function (event, d) {
+      tooltipSB.transition()
+        .duration(200)
+        .style("opacity", .9);
+
+      var genre = d3.select(this.parentNode).datum().genre;
+      var platformData = stackedData.find(g => g.genre === genre).platforms;
+      var platformDistribution = platformData.map(p => ({ platform: p.platform, count: p.count }));
+
+      tooltipSB.html(createPieChartHTML(platformDistribution, genre))
+        .style("left", (event.pageX) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    })
+    .on("mouseout", function (d) {
+      tooltipSB.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
+
+  // Add the x-axis
+  svgSB.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .style("fill", "white")
+    .attr("transform", "rotate(-30)")
+    .style("text-anchor", "end")
+    .attr("dx", "-0.8em")
+    .attr("dy", "0.15em");
+
+  // Add the y-axis
+  svgSB.append("g")
+    .attr("class", "axis")
+    .call(d3.axisLeft(y));
+});
